a55901e4bc3bca2490daa72c4379bdc03b9d9120
Dinegro Samuele <dinegro.samuele@osdbsesto>
2020-01-24 10:03:14 +0100

    add button and btn.eventListener

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.getElementById("id1").innerHTML="ciao come stai";
+var button= document.createElement("button");
+button.innerHTML="clicca";
+
+var body=document.getElementsByTagName("body")[0];
+body.append(button);
+
+button.addEventListener("click",function(){
+    document.getElementById("id1").innerHTML="ciao come stai";
+})
